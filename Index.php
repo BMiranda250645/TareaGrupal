87d5e92940987353115e0004972da9be026d88df
USP <USP@10.15.14.148>
2018-04-27 14:59:40 -0500

    Indexx

---
+++ Index.php
@@ -0,0 +1,9 @@
+<head>
+<title> OPERACIONES MATEMATICAS </title>
+</head>
+<body>
+<tr>
+    <td> <input type="submit" value="sumar"> </td>
+    <input type="submit" src="Suma.php" value="enviar"/>
+</tr>
+</body>

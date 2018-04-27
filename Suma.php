0b8336dd8c7c8ad6e61b9753582ab7444e8f98d8
USP <USP@10.15.14.148>
2018-04-27 14:32:49 -0500

    SUMA ENTEROS

--- Suma.php
+++ Suma.php
@@ -1,8 +1,36 @@
+<!DOCTYPE HTML>
+<html lang="es-ES">
+<head>
+	<meta charset="UTF-8">
+	<title>Suma de 2 numeros enteros</title>
+</head>
+<body>
+<H2>Suma de 2 numeros en PHP</H2>
+<form action="suma.php" method="POST">
+	<table>
+	<tr>
+		<td><input type="text" name="numero1"></td>
+	</tr>
+	<tr>
+		<td><input type="text" name="numero2"></td>
+	</tr>
+	<tr>
+		<td> <input type="submit" value="sumar"> </td>
+	</tr>
+</table>
+</form>
+</body>
+</html>
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+	if($_POST)
+	{	
+		$num1 = $_POST
+		['numero1'];
+		$num2 = $_POST
+		['numero2'];
+		$suma = $num1 
+		+ $num2;
+		echo "La suma de ".$num1." y ".$num2." es ".$suma; 
+	}
+?>
 

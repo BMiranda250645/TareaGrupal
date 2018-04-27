fc4ac82a31c584d8282929a78d2664bafc2fb275
USP <USP@C202-17>
2018-04-27 14:41:12 -0500

    MULTIPLICACIÓN

---
+++ Multiplicacion.php
@@ -0,0 +1,34 @@
+html lang="es-ES">
+<head>
+	<meta charset="UTF-8">
+	<title>Multiplicación de 2 numeros enteros</title>
+</head>
+<body>
+<H2>Multiplicación de 2 numeros en PHP</H2>
+<form action="multiplicar.php" method="POST">
+	<table>
+	<tr>
+		<td><input type="text" name="numero1"></td>
+	</tr>
+	<tr>
+		<td><input type="text" name="numero2"></td>
+	</tr>
+	<tr>
+		<td> <input type="submit" value="multiplicar"> </td>
+	</tr>
+</table>
+</form>
+</body>
+</html>
+<?php
+	if($_POST)
+	{	
+		$num1 = $_POST
+		['numero1'];
+		$num2 = $_POST
+		['numero2'];
+		$multiplicar = $num1 
+		* $num2;
+		echo "La multiplicacion de ".$num1." y ".$num2." es ".$multiplicar; 
+	}
+?>
